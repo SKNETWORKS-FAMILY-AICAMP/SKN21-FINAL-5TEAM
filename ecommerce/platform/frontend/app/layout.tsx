08d01c76229830ba08c65ec101123869e2fb46f5
whskadnd <whskadnd25@naver.com>
2026-02-10 09:10:59 +0900

    남웅_프론트_메인 페이지 챗봇 화면 구현

--- ecommerce/platform/frontend/app/layout.tsx
+++ ecommerce/platform/frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from './header/header';
-import ChatbotFab from "./chatbot/ChatbotFab";
+import ChatbotFab from "./chatbot/chatbotfab";
 
 const inter = Inter({ subsets: ["latin"] });
 
