03c9917ca99eea8a0e1b0c42ce961ddbb71a644d
whskadnd <whskadnd25@naver.com>
2026-03-16 12:38:33 +0900

    남웅_정책안내, 중고상품 등록, 주문목록 조회 해결

--- food/frontend/src/App.js
+++ food/frontend/src/App.js
@@ -5,7 +5,6 @@ import Header from "./components/Header";
 import Chatbot from "./components/chatbot";
 import ProductsPage from "./pages/Products";
 import LoginPage from "./pages/Login";
-import MyPage from "./pages/MyPage";
 import Orders from "./pages/Orders";
 import layout from "./styles/layout.module.css";
 import { AuthProvider } from "./context/AuthContext";
@@ -19,7 +18,6 @@ function App() {
           <Routes>
             <Route path="/" element={<ProductsPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/mypage" element={<MyPage />} />
             <Route path="/orders" element={<Orders />} />
           </Routes>
         </main>
